a63ec9eb334321fee7a0bdc8503552079e6b61a8
Rubens <rubens.andrade1@gmail.com>
2019-08-16 11:39:57 -0300

    Proj

---
+++ script.js
@@ -0,0 +1,7 @@
+let loginscroll = $('html, body');
+$('a').click(function() {
+    loginscroll.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
