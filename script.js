0792081e18fb79990e4b08088618c4f66c14f804
Rubens <40808539+Rubenscode@users.noreply.github.com>
2019-08-18 04:01:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let loginscroll = $('html, body');
-$('a').click(function() {
+$('a').click(function () {
     loginscroll.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
     return false;
 });
+
+
+function escrever(texto, idElemento, tempo) {
+    let letras = texto.split('').reverse();
+    let tipo = setInterval(function () {
+        if (!letras.length) return clearInterval(tipo);
+        let seguinte = letras.pop();
+        document.getElementsById(idElemento).innerHTML += seguinte;
+    }, tempo);
+}
+
+
+escrever('Proteja-se!, Proteja a sua Familía', 'log', 100);
